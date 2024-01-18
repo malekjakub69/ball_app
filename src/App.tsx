0f114676af11fb0e46ffbe411eb2a248c5747a5b
Jacob Malek <malekjakub69@gmail.com>
2024-01-18 08:38:13 +0100

    aaa

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+import './App.css'
 import { Card } from "./Card"
 
 export type ICard = {
@@ -8,8 +10,23 @@ export type ICard = {
 
 const cards: ICard[] = [
   {
-    cardPicture: "https://picsum.photos/200/300",
-    description: "Card 1",
+    cardPicture: "/bang.png",
+    description: "Bang!",
+    probability: 0.5
+  },
+  {
+    cardPicture: "/catling.png",
+    description: "Catling",
+    probability: 0.5
+  },
+  {
+    cardPicture: "/indians.png",
+    description: "Indians",
+    probability: 0.5
+  },
+  {
+    cardPicture: "/mancato.png",
+    description: "Mancato",
     probability: 0.5
   }
 ]
@@ -17,9 +34,30 @@ const cards: ICard[] = [
 
 function App() {
 
+  const [randomCard, setRandomCard] = useState<ICard>(cards[Math.round(Math.random()*(cards.length-1))])
+  
+  const startEffect = () => {
+    let cardIndex = 0;
+    let intervalId:number;
+    let delay = 60; // Počáteční rychlost procházení karet
+
+    const changeCard = () => {
+      setRandomCard(cards[cardIndex]);
+      cardIndex = (cardIndex + 1) % cards.length;
+      delay += delay*0.2; // Postupné zpomalení
+      intervalId = setTimeout(changeCard, delay);
+    };
+
+    changeCard();
+
+    setTimeout(() => {
+      clearTimeout(intervalId); // Zastavení procházení karet // Náhodný výběr karty
+    }, 3000); // Doba trvání efektu
+  };
   return (
     <>
-      <Card card={cards[0]} />
+      <Card card={randomCard} />
+      <button className="nextBtn" onClick={startEffect}>Generovat další kartu</button>
     </>
   )
 }
--- src/Card.tsx
+++ src/Card.tsx
@@ -10,17 +10,31 @@ interface IProps {
 export const Card: FC<IProps> = ({ card }) => {
     const [isFlipped, setIsFlipped] = useState(false);
 
-    const handleClick = () => {
+    const handleFlip = () => {
       setIsFlipped(!isFlipped);
     };
-    return <div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={handleClick}>
-      <div className="front">
-        <img src={card.cardPicture} alt="front" />
-      </div>
-      <div className="back">
-        <p>{card.probability}</p>
-      </div>
-    </div> 
+
+    return (
+    <div className="container">
+        <div
+            className={`flip-card ${
+                isFlipped ? "flipped" : ""
+            }`}
+        >
+            <div className="flip-card-inner">
+                <div onClick={handleFlip} className="flip-card-front">
+                    <div className="card-content">
+                        <img src={card.cardPicture} alt={card.description} />
+                    </div>
+                </div>
+                <div onClick={handleFlip} className="flip-card-back">
+                    <div className="card-content">
+                        {card.description}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>);
 }  
 
 
